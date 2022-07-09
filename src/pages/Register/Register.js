7cf450cb189c2584708f41e8a34af2d5c655f317
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-07-09 20:44:48 +0200

    Create Register page

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,21 @@
+import RegisterForm from "./RegisterForm/RegisterForm";
+import Logo from "../../components/UI/Logo/Logo";
+import styles from "./Register.module.scss";
+
+const Register = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <p>
+          Rent best cars from over <span>300</span> companies in Poland.
+        </p>
+      </div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Register;
