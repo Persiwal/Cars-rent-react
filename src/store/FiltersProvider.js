bc12262dea114fc70884678b3ec91c442046383f
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-07-09 20:38:53 +0200

    Create FiltersProvider

---
+++ src/store/FiltersProvider.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import FiltersContext from "./FiltersContext";
+
+const CarsFiltersProvider = (props) => {
+  const [filtersState, setFiltersState] = useState({
+    name: "",
+    isPromo: false,
+    isAvailableNow: true,
+    mark: "",
+    model: "",
+    minPrice: 0,
+    maxPrice: 10000,
+    minProdYear: 1970,
+    maxProdYear: 2022,
+  });
+
+  const filtersContextValue = {
+    name: filtersState.name,
+    isPromo: filtersState.isPromo,
+    isAvailableNow: filtersState.isAvailableNow,
+    mark: filtersState.mark,
+    model: filtersState.model,
+    minPrice: filtersState.minPrice,
+    maxPrice: filtersState.maxPrice,
+    minProdYear: filtersState.minProdYear,
+    maxProdYear: filtersState.maxProdYear,
+    setFiltersState,
+  };
+
+  return (
+    <FiltersContext.Provider value={filtersContextValue}>
+      {props.children}
+    </FiltersContext.Provider>
+  );
+};
+
+export default CarsFiltersProvider;
