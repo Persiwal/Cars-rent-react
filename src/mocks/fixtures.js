53635d7d07daf7fa02f869e4d7a14003c8a31492
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-07-09 20:36:27 +0200

    Create fixtures.js

---
+++ src/mocks/fixtures.js
@@ -0,0 +1,772 @@
+export const cars = [
+  {
+    basic: {
+      mark: "Chevrolet",
+      model: "Corvette",
+      body_type: "Coupe",
+      generation: "C7",
+      seats_number: 2,
+      production_year: 2014,
+      gearbox: "automatic",
+      photo_url:
+        "https://www.freeiconspng.com/uploads/land-rover-range-rover-car-png-25.png",
+    },
+    performance: {
+      fuel_consumption: 12.7,
+      acceleration_to_hundred: 3.8,
+      max_speed: 315,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 659,
+      torque: 881,
+      capacity: 6162,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+      {
+        author: "John Doe",
+        score: 7.4,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Volkswagen",
+      model: "Golf",
+      body_type: "Hatchback",
+      generation: "VII",
+      seats_number: 5,
+      gearbox: "automatic",
+      production_year: 2014,
+      photo_url: "https://www.freeiconspng.com/uploads/audi-png-auto-car-0.png",
+    },
+    performance: {
+      fuel_consumption: 12.7,
+      acceleration_to_hundred: 10.4,
+      max_speed: 140,
+      fuel_type: "eletric",
+    },
+    engine: {
+      power: 115,
+      torque: 270,
+      capacity: "not applicable",
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: true,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      generation: "8Y",
+      gearbox: "automatic",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url:
+        "https://www.freeiconspng.com/uploads/audi-jeep-q3-car-png-7.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 105,
+      promo: {
+        isPromo: true,
+        promoCost: 65,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Mercedes-Benz",
+      model: "CLS",
+      body_type: "Coupe",
+      generation: "C219",
+      seats_number: 4,
+      gearbox: "automatic",
+      production_year: 2008,
+      photo_url:
+        "https://www.freeiconspng.com/uploads/red-hyundai-car-png-15.png",
+    },
+    performance: {
+      fuel_consumption: 7.6,
+      acceleration_to_hundred: 7,
+      max_speed: 250,
+      fuel_type: "diesel",
+    },
+    engine: {
+      power: 224,
+      torque: 540,
+      capacity: 2987,
+    },
+    rent: {
+      costPerDay: 55,
+      promo: {
+        isPromo: false,
+        promoCost: 45,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "BMW",
+      model: "Series 7",
+      body_type: "Sedan",
+      generation: "G12",
+      gearbox: "automatic",
+      seats_number: 5,
+      production_year: 2016,
+      photo_url: "https://www.freeiconspng.com/uploads/car-png-27.png",
+    },
+    performance: {
+      fuel_consumption: 12.6,
+      acceleration_to_hundred: 3.9,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 600,
+      torque: 800,
+      capacity: 6592,
+    },
+    rent: {
+      costPerDay: 130,
+      promo: {
+        isPromo: true,
+        promoCost: 100,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      generation: "8Y",
+      gearbox: "automatic",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url:
+        "https://www.freeiconspng.com/uploads/lamborghini-car-png-4.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 140,
+      promo: {
+        isPromo: true,
+        promoCost: 120,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      generation: "8Y",
+      gearbox: "automatic",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 100,
+      promo: {
+        isPromo: true,
+        promoCost: 65,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      generation: "8Y",
+      gearbox: "automatic",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 95,
+      promo: {
+        isPromo: false,
+        promoCost: 65,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      generation: "8Y",
+      gearbox: "automatic",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 200,
+      promo: {
+        isPromo: true,
+        promoCost: 150,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: true,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "S3",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+  {
+    basic: {
+      mark: "Audi",
+      model: "A4",
+      body_type: "Hatchback",
+      gearbox: "automatic",
+      generation: "8Y",
+      seats_number: 5,
+      production_year: 2020,
+      photo_url: "https://www.freeiconspng.com/uploads/yellow-car-png-26.png",
+    },
+    performance: {
+      fuel_consumption: 7.4,
+      acceleration_to_hundred: 4.8,
+      max_speed: 250,
+      fuel_type: "gas",
+    },
+    engine: {
+      power: 310,
+      torque: 400,
+      capacity: 1984,
+    },
+    rent: {
+      costPerDay: 85,
+      promo: {
+        isPromo: false,
+        promoCost: 55,
+      },
+    },
+    opinions: [
+      {
+        author: "John Doe",
+        score: 8.3,
+        message:
+          "I rented this car for my company where i was driving with it 400km a day. I never had problems with it.",
+      },
+    ],
+  },
+];
